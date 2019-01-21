96d1847b7cbd13d13b6fcd04877394debd063649
Ragil Burhanudin Pamungkas <om.ragil7@gmail.com>
2019-01-21 21:51:44 +0700

    sip

--- sms.py
+++ sms.py
@@ -6,7 +6,8 @@ import urllib3
 import json
 def pesan(no,pesan):
     http = urllib3.PoolManager()
-    data ={"Content":{"phone_number": str(no),"message": str(pesan),"device_id": 107132}}
+    device_id_smsgatewayme = xxxxxxx
+    data ={"Content":{"phone_number": str(no),"message": str(pesan),"device_id": device_id_smsgatewayme}}
     encoded_data = json.dumps(data).encode('utf-8')
     r = http.request('POST', 'https://smsgateway.me/api/v4/message/send', headers={
         'Authorization' : 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJhZG1pbiIsImlhdCI6MTU0NTM5ODEwNCwiZXhwIjo0MTAyNDQ0ODAwLCJ1aWQiOjY1NjAwLCJyb2xlcyI6WyJST0xFX1VTRVIiXX0.M8wJhfR6RZ7WrELcFtM9AhTiW1ta4w7QgdCIHAVp6Zc','Content-Type': 'application/json'},
